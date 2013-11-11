41de06e8156987279aacac3b26505a14b94227cd
Kerry Knight <kerry.a.knight@gmail.com>
2013-11-11 15:59:39 -0500

    Added acknowledgements modal view; added CocoaLumberjack

---
+++ CalcuDates/View Controllers/KKSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  KKAboutThisAppViewController.h
+//  CalcuDates
+//
+//  Created by Kerry Knight on 11/11/13.
+//  Copyright (c) 2013 Kerry Knight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KKAboutThisAppViewController : UIViewController
+
+@end
