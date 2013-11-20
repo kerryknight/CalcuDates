2601b4ed9b035007986e2b06ae6317401124a066
Kerry Knight <kerry.a.knight@gmail.com>
2013-11-20 15:41:59 -0500

    Full test suite passed so far; setting up continuous integration with Mac OS X Server

---
+++ CalcuDatesTests/KKMainViewController+TestingExtensions.h
@@ -0,0 +1,13 @@
+//
+//  KKMainViewController+TestingExtensions.h
+//  CalcuDates
+//
+//  Created by Kerry Knight on 11/20/13.
+//  Copyright (c) 2013 Kerry Knight. All rights reserved.
+//
+
+#import "KKMainViewController.h"
+
+@interface KKMainViewController (TestingExtensions)
+
+@end
---
+++ CalcuDatesTests/KKMainViewController+TestingExtensions.m
@@ -0,0 +1,13 @@
+//
+//  KKMainViewController+TestingExtensions.m
+//  CalcuDates
+//
+//  Created by Kerry Knight on 11/20/13.
+//  Copyright (c) 2013 Kerry Knight. All rights reserved.
+//
+
+#import "KKMainViewController+TestingExtensions.h"
+
+@implementation KKMainViewController (TestingExtensions)
+
+@end
